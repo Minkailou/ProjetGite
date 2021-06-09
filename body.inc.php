a2427a04098fdd930892dcbd4d2752733f86df44
SACKO ( SACKO ) <81692775+Minkailou@users.noreply.github.com>
2021-06-09 16:58:38 +0200

    docslider

--- body.inc.php
+++ body.inc.php
@@ -4,33 +4,34 @@
        <div class="contenu-one">
         <div class="slider-wrapper">
             <ul class="slider">
-                <li class="slide"><img src="./img/slide5.jpg" alt="slide1"/></li>
-                <li class="slide"><img src="./img/slide4.jpg" alt="slide2"/></li>
-                <li class="slide"><img src="./img/slide2.jpg" alt="slide3"/></li>
-                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
-                <li class="slide"><img src="./img/slide1.jpg" alt="slide5"/></li>
-                <li class="slide"><img src="./img/slide5.jpg" alt="slide1"/></li>
-                <li class="slide"><img src="./img/slide4.jpg" alt="slide2"/></li>
-                <li class="slide"><img src="./img/slide2.jpg" alt="slide3"/></li>
-                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
-                <li class="slide"><img src="./img/slide1.jpg" alt="slide5"/></li>
+                <li class="slide"><img src="./img/villa1.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/villa2.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/maison1.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/maison2.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/maison3.jpg" alt="slide5"/></li>
+                <li class="slide"><img src="./img/maison4.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/maison5.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/maison6.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/maison7.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/maison8.jpg" alt="slide5"/></li>
             </ul>
          </div>
         </div>
         <div class="contenu-two">
         <div class="slider-wrapper">
             <ul class="slider">
-                <li class="slide"><img src="./img/slide1.jpg" alt="slide1"/></li>
-                <li class="slide"><img src="./img/slide2.jpg" alt="slide2"/></li>
-                <li class="slide"><img src="./img/slide4.jpg" alt="slide3"/></li>
-                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
-                <li class="slide"><img src="./img/slide5.jpg" alt="slide5"/></li>
-                <li class="slide"><img src="./img/slide1.jpg" alt="slide1"/></li>
-                <li class="slide"><img src="./img/slide4.jpg" alt="slide2"/></li>
-                <li class="slide"><img src="./img/slide2.jpg" alt="slide3"/></li>
-                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
-                <li class="slide"><img src="./img/slide5.jpg" alt="slide5"/></li>
+            <li class="slide"><img src="./img/maison8.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/maison7.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/maison6.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/maison5.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/maison4.jpg" alt="slide5"/></li>
+                <li class="slide"><img src="./img/maison3.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/maison2.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/maison1.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/villa2.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/villa1.jpg" alt="slide5"/></li>
             </ul>
          </div>
         </div>
+        <ul class="grid"
     </div>
