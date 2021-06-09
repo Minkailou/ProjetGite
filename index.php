4d04f947be24c0812d57ef84ca8393d201056364
SACKO ( SACKO ) <81692775+Minkailou@users.noreply.github.com>
2021-06-09 14:36:07 +0200

    nouveaudoc

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     </nav>
     <!------------- Contenu body ------------------>
     <div class="contenu">
-       
+       <div class="contenu-one">
         <div class="slider-wrapper">
             <ul class="slider">
                 <li class="slide"><img src="./img/slide5.jpg" alt="slide1"/></li>
@@ -39,6 +39,23 @@
                 <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
                 <li class="slide"><img src="./img/slide1.jpg" alt="slide5"/></li>
             </ul>
+         </div>
+        </div>
+        <div class="contenu-two">
+        <div class="slider-wrapper">
+            <ul class="slider">
+                <li class="slide"><img src="./img/slide1.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/slide2.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/slide4.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/slide5.jpg" alt="slide5"/></li>
+                <li class="slide"><img src="./img/slide1.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/slide4.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/slide2.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/slide5.jpg" alt="slide5"/></li>
+            </ul>
+         </div>
         </div>
     </div>
 
