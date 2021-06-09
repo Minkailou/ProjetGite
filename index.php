535a82e083287342907ea72eb8b1aec0ddfd0afa
SACKO ( SACKO ) <81692775+Minkailou@users.noreply.github.com>
2021-06-09 13:46:36 +0200

    gitecommit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <title>ProjetGite</title>
+     <meta charset="utf-8">
+     <link href="style.css" rel="stylesheet">
+     <link href="./img/logo.jpg" rel="icon">
+     <link href="./chemin-vers-fontawesome/css/all.css" rel="stylesheet">
+</head> 
+<body>
+    <input type="checkbox" id="menuBurger">
+    <label for="menuBurger"><i class="fa fa-bars" aria-hidden="true"></i></label>
+
+
+    <nav role="navigation" id="navMenu">
+       
+       <ul> 
+          <img class="log-menu" src="./img/discover.png" alt="logo">
+          <li><a href="#">Paris</a></li>
+          <li><a href="#">Rome</a></li>
+          <li><a href="#">Athens</a></li>
+          <li><a href="#">Lisbonne</a></li>
+          <li><a href="#">Madrid</a></li>
+        </ul>
+    </nav>
+    <!------------- Contenu body ------------------>
+    <div class="contenu">
+       
+        <div class="slider-wrapper">
+            <ul class="slider">
+                <li class="slide"><img src="./img/slide5.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/slide4.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/slide2.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/slide1.jpg" alt="slide5"/></li>
+                <li class="slide"><img src="./img/slide5.jpg" alt="slide1"/></li>
+                <li class="slide"><img src="./img/slide4.jpg" alt="slide2"/></li>
+                <li class="slide"><img src="./img/slide2.jpg" alt="slide3"/></li>
+                <li class="slide"><img src="./img/slide3.jpg" alt="slide4"/></li>
+                <li class="slide"><img src="./img/slide1.jpg" alt="slide5"/></li>
+            </ul>
+        </div>
+    </div>
+
+    <footer>
+        
+          <!-- <p id="paragraphe">Copyright&copy;2021</p> -->
+       
+           <ul class="navigation" role="navigation">
+               <li><a href="#"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a></li>
+               <li><a href="#"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a></li>
+               <li><a href="#"><i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a></li>
+               
+         
+           </ul>
+    </footer>
+    <script src="https://use.fontawesome.com/fe6d492c4a.js"></script>
+</body>
+</html>
